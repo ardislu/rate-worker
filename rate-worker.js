93b37e7f24ccf5f009a54a527968d29316b78d4f
Ardis Lu <ardisglu@gmail.com>
2024-09-23 22:07:14 -0700

    Use messages instead of search parameters for input
    Also enables multiple throttles with different configurations to be active at one time.

--- rate-worker.js
+++ rate-worker.js
@@ -1,51 +1,74 @@
-const params = new URLSearchParams(location.search);
-const limitedHostnames = params.get('limitedHostnames')?.split(',') ?? []; // Required to pass at least one hostname otherwise worker will not do anything
-const limitedMethods = params.get('limitedMethods')?.split(',') ?? ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']; // Excluding HEAD, CONNECT, OPTIONS, TRACE
-const maxConcurrentRequests = Number(params.get('maxConcurrentRequests')) ?? 10;
-const sleepDuration = Number(params.get('sleepDuration')) ?? 1000;
-const batchInterval = Number(params.get('batchInterval')) ?? 100;
+/**
+ * @typedef {Object} ThrottleOptions
+ * @property {string|Array<string>} hostname A hostname or array of hostnames that this throttling applies to. If multiple hostnames are provided, all hostnames share the same throttle.
+ * @property {(request: Request) => boolean} [shouldThrottle] Function that returns `true` if a given request should be throttled or `false` if not.
+ * @property {number} [maxConcurrentRequests=10] The maximum number of requests that are allowed to be inflight at the same time.
+ * @property {number} [sleepDuration=1000] The duration (in milliseconds) to wait in between sending each batch of requests.
+ * @property {number} [batchInterval=100] The duration (in milliseconds) to wait before sending the first batch of requests.
+ */
+
+const throttles = [];
 
 async function sleep(ms) {
   return new Promise(r => setTimeout(r, ms));
 }
 
-const pending = [];
-const inflight = Array(maxConcurrentRequests).fill(false);
-let running = false;
-async function clearPending() {
-  if (running) { // Only allow one instance of clearPending() to run at a time
+async function clearPending(throttle) {
+  if (throttle.running) { // Only allow one instance of clearPending() to run at a time per throttle
     return;
   }
-  running = true;
+  throttle.running = true;
   while (true) {
-    for (let i = 0; i < inflight.length; i++) {
-      if (!inflight[i]) {
-        inflight[i] = true;
-        const { request, resolve } = pending.shift();
+    for (let i = 0; i < throttle.inflight.length; i++) {
+      if (!throttle.inflight[i]) {
+        throttle.inflight[i] = true;
+        const { request, resolve } = throttle.pending.shift();
         (async () => {
           await fetch(request).then(resolve);
-          inflight[i] = false;
+          throttle.inflight[i] = false;
         })();
-        if (pending.length === 0) { // This check needs to be nested in here because .shift mutates the length
-          running = false;
+        if (throttle.pending.length === 0) { // This check needs to be nested in here because .shift mutates the length
+          throttle.running = false;
           return;
         }
       }
     }
-    await sleep(sleepDuration);
+    await sleep(throttle.sleepDuration);
   }
 }
 
-async function delayedFetch(request) {
+async function delayedFetch(request, throttle) {
   const { promise, resolve } = Promise.withResolvers();
-  pending.push({ request, resolve });
-  setTimeout(clearPending, batchInterval); // Use a short first sleep to gather initial requests, otherwise the first request will start immediately and then invoke the longer sleepDuration
+  throttle.pending.push({ request, resolve });
+  setTimeout(() => clearPending(throttle), throttle.batchInterval); // Use a short first sleep to gather initial requests, otherwise the first request will start immediately and then invoke the longer sleepDuration
   return promise;
 }
 
+addEventListener('message', e => {
+  const inputs = Array.isArray(e.data) ? e.data : [e.data];
+  for (const throttle of inputs) {
+    // Set defaults and clean up user input
+    throttle.hostname = Array.isArray(throttle.hostname) ? throttle.hostname : [throttle.hostname];
+    throttle.shouldThrottle ??= (request) => ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'].includes(request.method);
+    throttle.maxConcurrentRequests ??= 10;
+    throttle.sleepDuration ??= 1000;
+    throttle.batchInterval ??= 100;
+
+    // Set parameters for clearPending
+    throttle.pending = [];
+    throttle.inflight = Array(throttle.maxConcurrentRequests).fill(false);
+    throttle.running = false;
+
+    throttles.push(throttle);
+  }
+});
+
 addEventListener('fetch', e => {
-  if (limitedHostnames.includes(new URL(e.request.url).hostname) && limitedMethods.includes(e.request.method)) {
-    e.respondWith(delayedFetch(e.request));
+  for (const throttle of throttles) {
+    if (throttle.hostname.includes(new URL(e.request.url).hostname) && throttle.shouldThrottle(e.request)) {
+      e.respondWith(delayedFetch(e.request, throttle));
+      break; // If multiple throttles apply to a request, only the first throttle has an effect
+    }
   }
 });
 
